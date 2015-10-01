25ac264aa49e8f71f36595c7ec97ef0a45f9eadb
Rodrigo <Rodrigo@10.10.247.213>
2015-10-01 16:03:52 -0300

    Criação da estrutura básica da classe ItensVenda.

---
+++ ExemploEstoque/ItensVenda.php
@@ -0,0 +1,32 @@
+<?php
+
+class ItensVenda {
+    public $data;
+    public $item;
+    public $quantidade;
+    public $venda;
+
+
+    public function inicializa() {
+    }
+    
+    public function alocaVenda() {
+        
+    }
+    
+    public function devolveProduto() {
+        
+    }
+    
+    public function localizaCodigo() {
+        
+    }
+    
+    public function printOn() {
+        
+    }
+    
+    public function removeVenda() {
+        
+    }
+}
