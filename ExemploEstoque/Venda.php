60e78a0cfca2872b2093092de42e8a8e70465235
Rodrigo <Rodrigo@10.10.247.213>
2015-10-01 15:55:36 -0300

    Criação do projeto. Criação da estrutura básica do index e da classe Venda.

---
+++ ExemploEstoque/Venda.php
@@ -0,0 +1,33 @@
+<?php
+
+class Venda {
+    public $codigo;
+    public $data;
+    public $hora;
+    public $itensVenda;
+        
+    public function inicializa() {
+        $this->data = date('d-m-Y');
+        $this->hora = date('H:i');
+    }
+    
+    public function adicionaItem() {
+        
+    }
+    
+    public function printOn() {
+        
+    }
+    
+    public function removeItem() {
+        
+    }
+    
+    public function removeItens() {
+        
+    }
+    
+    public function verificaVenda() {
+        
+    }
+}
---
+++ ExemploEstoque/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<?php
+    require_once './Venda.php';
+?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        ?>
+    </body>
+</html>
