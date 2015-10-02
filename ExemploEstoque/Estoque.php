5ddc2ebde0b0209241f7cf73f0d412fac9aeef60
Rodrigo <Rodrigo@DESKTOP-S5LR89K>
2015-10-01 22:05:22 -0300

    Criação da estrutura básica da classe Estoque e ajuste de identação da classe ItensVenda

---
+++ ExemploEstoque/Estoque.php
@@ -0,0 +1,45 @@
+<?php
+
+class Estoque {
+
+    private $empresa;
+    private $listaDeProdutos;
+    private $listaDeVendas;
+
+    public function inicializa() {
+        
+    }
+
+    public function adicionaProduto() {
+        
+    }
+
+    public function adicionaVenda() {
+        
+    }
+
+    public function consultaProduto() {
+        
+    }
+
+    public function consultaVenda() {
+        
+    }
+
+    public function imprimeProdutos() {
+        
+    }
+
+    public function imprimeVendas() {
+        
+    }
+
+    public function removeProduto() {
+        
+    }
+
+    public function removeVenda() {
+        
+    }
+
+}
--- ExemploEstoque/ItensVenda.php
+++ ExemploEstoque/ItensVenda.php
@@ -1,32 +1,34 @@
 <?php
 
 class ItensVenda {
+
     private $data;
     private $item;
     private $quantidade;
     private $venda;
 
-
     public function inicializa() {
+        
     }
-    
+
     public function alocaVenda() {
         
     }
-    
+
     public function devolveProduto() {
         
     }
-    
+
     public function localizaCodigo() {
         
     }
-    
+
     public function printOn() {
         
     }
-    
+
     public function removeVenda() {
         
     }
+
 }
