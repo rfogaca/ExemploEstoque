b973c3af77c9d516c5d85a9e185e80eb73718d49
Rodrigo <Rodrigo@DESKTOP-S5LR89K>
2015-10-01 22:15:52 -0300

    Criação da estrutura básica da classe Preco.

---
+++ ExemploEstoque/Preco.php
@@ -0,0 +1,30 @@
+<?php
+
+class Preco {
+
+    private $dataInicial;
+    private $dataFinal;
+    private $item;
+    private $preco;
+
+    public function inicializa($dataInicial, $dataFinal) {
+        
+    }
+
+    public function alocaItem() {
+        
+    }
+
+    public function imprimePreco() {
+        
+    }
+
+    public function localizaPreco() {
+        
+    }
+
+    public function printOn() {
+        
+    }
+
+}
